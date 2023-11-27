37422af964a8d4de15b4d9031203278a33eba587
Anurag Yadav <anuragyadavmnp@gmail.com>
2023-11-27 16:22:48 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
         <motion.section className="home" {...slideAnimation('left')}>
           <motion.header {...slideAnimation("down")}>
             <img 
-              src='./threejs.png'
+              src='./vibacious.png'
               alt="logo"
               className="w-8 h-8 object-contain"
             />
@@ -28,7 +28,7 @@ const Home = () => {
           <motion.div className="home-content" {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
               <h1 className="head-text">
-                LET'S <br className="xl:block hidden" /> DO IT.
+                LET'S <br className="xl:block hidden" /> CUSTOMIZE IT.
               </h1>
             </motion.div>
             <motion.div
@@ -36,12 +36,12 @@ const Home = () => {
               className="flex flex-col gap-5"
             >
               <p className="max-w-md font-normal text-gray-600 text-base">
-              Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong>{" "} and define your own style.
+              Design your custom and exclusive shirt with 360 view customization tool. <strong>Where Trend Meets Tailored</strong>{" "} and Style is Uniquely Yours.
               </p>
 
               <CustomButton 
                 type="filled"
-                title="Customize It"
+                title="Start Customizing"
                 handleClick={() => state.intro = false}
                 customStyles="w-fit px-4 py-2.5 font-bold text-sm"
               />
